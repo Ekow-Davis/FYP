d7831b46bbf7c47699b4c57daa30af2a000cf8b8
David Berko <daberko364@gmail.com>
2026-05-04 16:11:55 +0000

    feature extraction block modified to match paper

--- models/lead_cnn/architecture.py
+++ models/lead_cnn/architecture.py
@@ -3,13 +3,13 @@ from tensorflow.keras import layers, models
 from dimension_reduction_block import dimension_reduction_block
 from config import IMG_SIZE, NUM_CLASSES
 
-
-def conv_block(x, filters, block_id):
-  x = layers.Conv2D(filters, (3, 3), padding='same', name=f"conv_{block_id}")(x)
-  x = layers.LeakyReLU(alpha=0.1, name=f"leakyrelu_{block_id}")(x)
-  x = layers.MaxPooling2D(pool_size=(2, 2), name=f"pool_{block_id}")(x)
-  x = layers.Dropout(0.25, name=f"dropout_{block_id}")(x)
-  return x
+# The paper does not follow a single repeating pattern so this can be removed
+# def conv_block(x, filters, block_id):
+#   x = layers.Conv2D(filters, (3, 3), padding='same', name=f"conv_{block_id}")(x)
+#   x = layers.LeakyReLU(alpha=0.1, name=f"leakyrelu_{block_id}")(x)
+#   x = layers.MaxPooling2D(pool_size=(2, 2), name=f"pool_{block_id}")(x)
+#   x = layers.Dropout(0.25, name=f"dropout_{block_id}")(x)
+#   return x
 
 
 def build_lead_cnn():
@@ -17,14 +17,48 @@ def build_lead_cnn():
 
 
   # Feature extraction blocks
-  x = conv_block(inputs, 32, 1)
-  x = conv_block(x, 64, 2)
-  x = conv_block(x, 128, 3)
-  x = conv_block(x, 256, 4)
+  # x = conv_block(inputs, 32, 1)
+  # x = conv_block(x, 64, 2)
+  # x = conv_block(x, 128, 3)
+  # x = conv_block(x, 256, 4)
+
+
+  #New feature extraction block
+  #Conv1: 3x3, 32 filters - LReLU - MaxPool (no dropout)
+  x = layers.Conv2D(32, (3,3,), padding='valid', name="conv_1")(inputs)
+  x = layers.LeakyReLU(alpha=0.2, name="leakyrelu_1")(x)
+  x = layers.MaxPooling2D(pool_size=(2,2), name="pool_1")(x)
+
+  #Conv2: 3x3, 32 filters - LReLU - MaxPool - Dropout
+  x = layers.Conv2D(32, (3,3), padding="valid", name="conv_2")(x)
+  x = layers.LeakyReLU(alpha=0.2, name="leakyrelu_2")(x)
+  x = layers.MaxPooling2D(pool_size=(2,2), name="pool_2")(x)
+  x = layers.Dropout(0.25, name="dropout_1")(x)
+
+  #Conv3: 3x3, 64 filters - LReLU (no pool)
+  x = layers.Conv2D(64, (3,3), padding="valid", name="conv_3")(x)
+  x = layers.LeakyReLU(alpha=0.2, name="leakyrelu_3")(x)
+
+  #Conv4: 3x3, 64 filters - LReLU - MaxPool - Dropout
+  x = layers.Conv2D(64, (3,3), padding="valid", name="conv_4")(x)
+  x = layers.LeakyReLU(alpha=0.2, name="leakyrelu_4")(x)
+  x = layers.MaxPooling2D(pool_size=(2,2), name="pool_3")(x)
+  x = layers.Dropout(0.25, name="dropout_2")(x)
+
+  #Conv5: 3x3, 128 filters - LReLU (no pool)
+  x = layers.Conv2D(128, (3,3), padding="valid", name="conv_5")(x)
+  x = layers.LeakyReLU(alpha=0.2, name="leakyrelu_5")(x)
+
+  #Conv6: 3x3, 128 filters - LReLU - MaxPool - Dropout
+  x = layers.Conv2D(128, (3,3), padding="valid", name="conv_6")(x)
+  x = layers.LeakyReLU(alpha=0.2, name="leakyrelu_6")(x)
+  x = layers.MaxPooling2D(pool_size=(2,2), name="pool_4")(x)
+  x = layers.Dropout(0.25, name="dropout_3")(x)
+
 
 
   # Dimension reduction block (paper's contribution)
-  x = dimension_reduction_block(x, filters=64)
+  x = dimension_reduction_block(x, filters=16)
 
 
   # Classification head
